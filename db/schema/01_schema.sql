030a8d3edc18a6e49ab4c1888a3a0939107d43f3
Markjust1 <novikov711@gmail.com>
2022-06-04 12:14:05 -0600

    modify schema and seeds

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -24,5 +24,5 @@ CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   order_time TIMESTAMP NOT NULL,
-  item_id
+  item_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -15,21 +15,3 @@ INSERT INTO items (item, description, price, cooking_time, thumbnail_photo) VALU
   ('Pickled fish in a bucket', 'Straight from the docks and into the brine', 4000, 3, 'http://img' ),
   ('Pork?', 'Your guess is as good as ours', 300, 10, 'http://img'),
   ('Seared burrito', 'Unexpected consequences', 600, 15, 'http://img');
-
-  INSERT INTO orders (user_id, order_time) VALUES
-  (2, TIMESTAMP),
-  (3, TIMESTAMP),
-  (3, TIMESTAMP),
-  (4, TIMESTAMP),
-  (5, TIMESTAMP),
-  (6, TIMESTAMP);
-
-  INSERT INTO order_items (order_id, item_id, quantity) VALUES
-  ( 1, '7783208267', 'funkyfuuds@yahoo.com'),
-  ( 2, '7783208267', 'bobby@yahoo.com'),
-  ( 3, '7967576489', 'duggy@yahoo.com'),
-  ( 4, '4035677893', 'ruff@yahoo.com'),
-  ( 5, '7789808765', 'oink@yahoo.com'),
-  ( 6, '6045831234', 'fish@yahoo.com');
-
-orders.id = order_id
