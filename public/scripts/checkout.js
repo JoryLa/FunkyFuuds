967b04d6d3dbcf46b312d27fe216708c3b27fd1d
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-06-04 22:11:24 -0700

    css styling updates

---
+++ public/scripts/checkout.js
@@ -0,0 +1,36 @@
+const menuItems = (items) => {
+  console.log(items);
+  const menuTemplate = `
+  <article class="food">
+  <section class="thumbnail">
+  <img src="${items.thumbnail_photo}">
+      </section>
+  <div class="content">
+  <li> ${items.item}</li>
+    <p class="description">
+  <li> ${items.description}
+  </p>
+  </div>
+  <div class="add">
+  <button type="button" class="btn btn-danger">Remove</button>
+  <div class="button">
+  <button id="plus">+</button>
+  <input type="number" value="0" id="input" />
+  <button id="minus">−</button>
+  </div>
+  </div>
+  </article>
+  `;
+  $("#food-list").append(menuTemplate);
+};
+const loadmenu = () => {
+  $('#food-list').empty()
+  $.get("http://localhost:8080/api/food_items/checkout", (res) => {
+    console.log("res", res);
+    for (let key of res.orderItems) {
+      console.log("key", key);
+      menuItems(key);
+    }
+  });
+};
+loadmenu()
