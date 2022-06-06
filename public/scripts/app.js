df7380fe457e2f99c533d117ced1389399db4739
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-06-05 19:18:37 -0700

    working plus minus buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,24 @@
 // Client facing scripts here
 
-
-
 const menuItems = (items) => {
-  console.log(items);
   const menuTemplate = `
   <article class="food">
   <section class="thumbnail">
   <img src="${items.thumbnail_photo}">
       </section>
   <div class="content">
-  <li> ${items.item}</li>
-    <p class="description">
-  <li> ${items.description}
+  <p> ${items.item}
+  </p>
+  <p class="description">
+<p> ${items.description}
+</p>
   </p>
   </div>
   <div class="add">
   <button type="button" class="btn btn-danger">Add to Cart</button>
+  <div>
+  $${items.price / 100}
+  </div>
   <div class="button">
   <button id="plus">+</button>
   <input type="number" value="0" id="input" />
@@ -28,13 +30,43 @@ const menuItems = (items) => {
   $("#food-list").append(menuTemplate);
 };
 const loadmenu = () => {
-  $('#food-list').empty()
+  $("#food-list").empty();
   $.get("http://localhost:8080/api/food_items", (res) => {
     for (let key of res.foodItems) {
       menuItems(key);
     }
   });
 };
-loadmenu()
+loadmenu();
 
+$(document).ready(() => {
 
+  $("#plus").click(function () {
+    console.log("clicked");
+  num = parseInt($("#input").val());
+  $("#input").val(num + 1);
+  });
+
+});
+
+$(document).ready(() => {
+  $("#minus").click(function () {
+    console.log("clicked");
+  num = parseInt($("#input").val());
+  $("#input").val(num - 1);
+  });
+})
+//
+
+// var $input = $("#input");
+
+// $input.val(0);
+
+// $(".button").click(function(){
+//   console.log("push the button")
+//     if ($(this).hasClass('plus')){
+//         $input.val(parseInt($input.val())+1);
+//     }else if ($input.val()>=1) {
+//         $input.val(parseInt($input.val())-1);
+//     }
+// });
--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,4 +1,4 @@
-const menuItems = (items) => {
+const orderItems = (items) => {
   console.log(items);
   const menuTemplate = `
   <article class="food">
@@ -6,19 +6,24 @@ const menuItems = (items) => {
   <img src="${items.thumbnail_photo}">
       </section>
   <div class="content">
-  <li> ${items.item}</li>
+  <title> ${items.item}</title>
     <p class="description">
-  <li> ${items.description}
+  <p> ${items.description}
+  </p>
   </p>
   </div>
   <div class="add">
   <button type="button" class="btn btn-danger">Remove</button>
+  <div>
+  $${items.price/100}
+  </div>
   <div class="button">
   <button id="plus">+</button>
   <input type="number" value="0" id="input" />
   <button id="minus">−</button>
   </div>
   </div>
+
   </article>
   `;
   $("#food-list").append(menuTemplate);
@@ -29,8 +34,9 @@ const loadmenu = () => {
     console.log("res", res);
     for (let key of res.orderItems) {
       console.log("key", key);
-      menuItems(key);
+      orderItems(key);
     }
   });
 };
 loadmenu()
+
