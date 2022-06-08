6a5154bb87c4255c42019843d88d8714801ea3ad
Markjust1 <novikov711@gmail.com>
2022-06-08 12:10:28 -0600

    database fix

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -5,7 +5,11 @@ INSERT INTO users (name, phone_number, email) VALUES
   ('Doug', '7967576489', 'duggy@yahoo.com'),
   ('Bulldog', '4035677893', 'ruff@yahoo.com'),
   ('Porky', '7789808765', 'oink@yahoo.com'),
-  ('Fish', '6045831234', 'fish@yahoo.com');
+  ('Fish', '6045831234', 'fish@yahoo.com'),
+  ('Blob', '4205672348', 'blobby@yahoo.com'),
+  ('Jim', '6549876754', 'jim@yahoo.com'),
+  ('Dwight', '1231231212', 'dwight@yahoo.com'),
+  ('Oscar', '9878766543', 'oscar@yahoo.com');
 
 
 INSERT INTO items (item, description, price, cooking_time, thumbnail_photo) VALUES
@@ -14,11 +18,11 @@ INSERT INTO items (item, description, price, cooking_time, thumbnail_photo) VALU
   ('Stuffed animal', '????', 3000, 10, '../../docs/stuffed_animal.jpeg'),
   ('Pickled fish in a bucket', 'Straight from the docks and into the brine', 4000, 3, '../../docs/fish.jpeg' ),
   ('Pork?', 'Your guess is as good as ours', 300, 5, '../../docs/pork.jpeg'),
-  ('Seared burrito', 'Unexpected consequences', 600, 4, ''),
-  ('Angry Perogies', 'Proceed with caution', 800, 8, ''),
-  ('Furry Bagel', 'No questions asked', 250, 3, ''),
-  ('Crunchy farmers wrap', 'For your dog maybe?', 150, 4, ''),
-  ('Aged Crème Brulée', 'Chefs special', 300, 2, '');
+  ('Seared burrito', 'Unexpected consequences', 600, 4, '../../docs/burrito.jpeg'),
+  ('Angry Perogies', 'Proceed with caution', 800, 8, '../../docs/burrito.jpeg'),
+  ('Furry Bagel', 'No questions asked', 250, 3, '../../docs/burrito.jpeg'),
+  ('Crunchy farmers wrap', 'For your dog maybe?', 150, 4, '../../docs/burrito.jpeg'),
+  ('Aged Crème Brulée', 'Chefs special', 300, 2, '../../docs/burrito.jpeg');
 
   -- INSERT INTO orders (user_id, item_id) VALUES
   -- (2, 3),
