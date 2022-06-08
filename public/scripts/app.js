7ec83215f7cd3acb2f69ff84630cc1763fe84f1d
Markjust1 <novikov711@gmail.com>
2022-06-08 17:32:14 -0600

    egf

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -66,22 +66,23 @@ $(document).ready(function () {
     if (cart[id] === 10) {
       $(this).parent().find(".plus").prop("disabled", true);
     }
-    // if(cart[id] === 9){
-    //   $(".plus").prop("disabled", false);
-    // }
+    if ($(this).parent().find(".minus").prop("disabled", true)) {
+      $(this).parent().find(".minus").prop("disabled", false)
+    }
 
     updateCart(cart);
   });
 
   $("#food-list").on("click", ".minus", function () {
     const id = $(this).attr("data-key");
-    console.log(cart[id]);
+    // console.log(cart[id]);
     if (!cart[id]) {
-      cart[id] = 0;
+      cart[id] = 1;
     }
     cart[id]--;
 
     num = parseInt($(this).parent().find(".input").val());
+    console.log("num",num)
     if (num > 0) {
       $(this)
         .parent()
@@ -91,7 +92,10 @@ $(document).ready(function () {
     if (cart[id] === 0) {
       $(this).parent().find(".minus").prop("disabled", true)
     }
-    // .change();
+    if ($(this).parent().find(".plus").prop("disabled", true)) {
+      $(this).parent().find(".plus").prop("disabled", false)
+    }
+
     updateCart(cart);
   });
 
@@ -132,9 +136,26 @@ $(document).ready(function () {
 
 `);
             container.append($element);
+
+
             if (cart[key] === 0) {
-              $element.empty();
+             $element.empty()
+            //  delete
             }
+            // if (cart[key] === 0){
+            //   cart.remove(cart[key])
+            // }
+            // if ($element === $element.empty()){
+            //   $("cart").empty()
+            // }
+            console.log("cart",cart)
+            console.log("key", key)
+            console.log("cart[key]",cart[key])
+            // let keyByCart = key*key
+            // console.log("sum on cart[key]", cart[key])
+            // if (keyByCart === 0){
+            //   $(".outsideCart").hide();
+            // }
             cookTime += item.cooking_time * cart[key];
             // console.log("cookTime", cookTime);
             total += (item.price * cart[key]) / 100;
