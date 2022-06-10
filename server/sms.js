ed9318e9f9f159e9e3195f7282aba25700d0bdb9
JorLo <jory.laurin@gmail.com>
2022-06-09 19:30:29 -0700

    hides auth

--- server/sms.js
+++ server/sms.js
@@ -1,5 +1,5 @@
-const accountSid = 'ACaba4da5e64dd74c28b925c77b6467b25';
-const authToken = '91fe56358a766e4e64eafc28390f0ab6';
+const accountSid = '';
+const authToken = '';
 const client = require('twilio')(accountSid, authToken);
 
 
