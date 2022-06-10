3de4435306bc19a8a0c0a8f030690576e035bb1d
JorLo <jory.laurin@gmail.com>
2022-06-09 19:29:03 -0700

    condensed schema

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -2,12 +2,11 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS items CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
-DROP TABLE IF EXISTS orders_by_users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(255) NOT NULL
+  phone_number VARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE items (
@@ -22,14 +21,7 @@ CREATE TABLE items (
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  order_time TIMESTAMP NOT NULL DEFAULT Now(),
+  order_time TIMESTAMP NOT NULL DEFAULT Now()
   item_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  quantity INTEGER NOT NULL DEFAULT 1
+  quantity INTEGER NOT NULL DEFAULT 1,
 );
-
-CREATE TABLE orders_by_users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE
-);
-
--- SELECT orders.*, orders_by_users.id as order_num FROM orders JOIN orders_by_users ON orders.id = order_id;
